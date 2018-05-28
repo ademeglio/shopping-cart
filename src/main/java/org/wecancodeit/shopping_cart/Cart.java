91b9853d7c1748e89ba9dfebef07ce83026825c5
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-05-28 09:45:30 -0400

    create getTotalCost() method and test good; develop  showOrder();

--- src/main/java/org/wecancodeit/shopping_cart/Cart.java
+++ src/main/java/org/wecancodeit/shopping_cart/Cart.java
@@ -1,24 +1,73 @@
 package org.wecancodeit.shopping_cart;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Cart {
-
-	public void addItem(Item item) {
-
-	// getters
-	
-	// setters
+	List<Item> orders = new ArrayList<>();
 	
 	// add items to the cart
-		// TODO Auto-generated method stub
+	public void addItem(Item item) {
+		orders.add(item);
+	}
+
+	// iterate through items and sum item quantities
+	public int getTotalItems() {
+		int itemsQty = 0;
 		
+		for (Item item : orders) {              
+			itemsQty += item.getItemQty();
+		}
+
+		return itemsQty;
+	}
+
+	// remove items from the cart
+	public void removeItem(String itemName) {
+		for (int i = orders.size() - 1; i >= 0; i--) {
+			if (orders.get(i).getItemName().equals(itemName)) {
+				orders.remove(i);
+			}			
+		} // end for
+	}
+
+	// total cost of all items in the cart
+	public String getTotalCost() {
+		double cartTotalCharge = 0;
+		
+		for (Item item : orders) {
+			cartTotalCharge += item.getItemPrice() * item.getItemQty();
+		}
+		
+		BigDecimal amount = new BigDecimal(cartTotalCharge);
+		NumberFormat n = NumberFormat.getCurrencyInstance();
+		double doubleAmount = amount.doubleValue();
+		String amountToCurrency = n.format(doubleAmount);
+		
+		return amountToCurrency;
 	}
 	
-	// remove items from the cart
-	
-	// total items in the the cart
-	
-	// total cost of all items in the cart
+	public String getUnitExtendedPrice(Item item) {
+		double unitExtendedPrice = item.getItemPrice() * item.getItemQty();
+		
+		BigDecimal amount = new BigDecimal(unitExtendedPrice);
+		NumberFormat n = NumberFormat.getCurrencyInstance();
+		double doubleAmount = amount.doubleValue();
+		String amountToCurrency = n.format(doubleAmount);
+		
+		return amountToCurrency;
+	}
 	
 	// traverse and show contents of the cart
-	
+	public void showOrder() {
+		for (Item item : orders) {
+			System.out.println("ITEM: " + item.getItemName() 
+					+ " UNIT PRICE:" + item.getItemPrice()
+					+ " QUANTITY: " + item.getItemQty()
+					+ " EXTENDED PRICE: " + getUnitExtendedPrice(item) );
+		}
+	}
 }
--- src/test/java/org/wecancodeit/shopping_cart/CartTest.java
+++ src/test/java/org/wecancodeit/shopping_cart/CartTest.java
@@ -10,9 +10,50 @@ public class CartTest {
 	public void shouldReturnTotalItemsAsOne() {
 		Cart underTest = new Cart();
 		underTest.addItem(new Item("", 1.99, 1));
-		int itemsTotal = underTest.getTotalItems();
+		int itemsQty = underTest.getTotalItems();
 		
-		assertEquals(itemsTotal, 1);
+		assertEquals(1, itemsQty);
 		
 	}
-}
+	
+	@Test
+	public void shouldReturnTotalItemsAsTwo() {
+		Cart underTest = new Cart();
+		underTest.addItem(new Item("alternator", 15.50, 1));
+		underTest.addItem(new Item("plug", 1.50, 2));
+		int itemsQty = underTest.getTotalItems();
+		
+		assertEquals(3, itemsQty);
+	}
+	
+	@Test
+	public void shouldReturnTotalItemsAsOneAfterRemovingItem() {
+		Cart underTest = new Cart();
+		underTest.addItem(new Item("alternator", 15.50, 1));
+		underTest.addItem(new Item("plug", 1.50, 2));
+		underTest.removeItem("plug");
+		int itemsQty = underTest.getTotalItems();
+		
+		assertEquals(1, itemsQty);
+	}
+	
+	@Test
+	public void shouldReturnTotalCostOfCartItems() {
+		Cart underTest = new Cart();
+		underTest.addItem(new Item("alternator", 15.51, 1));
+		underTest.addItem(new Item("plug", 1.50, 2));
+		String itemsCost = underTest.getTotalCost();
+		
+		assertEquals("$18.51", itemsCost);
+	}
+	
+	@Test
+	public void shouldReturnTotalCostOfCartItemsTest2() {
+		Cart underTest = new Cart();
+		underTest.addItem(new Item("alternator", 15.51, 3));
+		underTest.addItem(new Item("plug", 1.53, 2));
+		String itemsCost = underTest.getTotalCost();
+		
+		assertEquals("$49.59", itemsCost);
+	}
+} //end cart class
