e9bb4566104649b5dc629514b74dcb536727478f
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-05-28 10:29:49 -0400

    initial development ShopApp()

---
+++ src/main/java/org/wecancodeit/shopping_cart/ShopApp.java
@@ -0,0 +1,52 @@
+package org.wecancodeit.shopping_cart;
+
+import java.util.Scanner;
+
+public class ShopApp {
+
+	public static void main(String[] args) {
+		
+		boolean isDonePurchasing = false;
+		Scanner userInput = new Scanner(System.in);
+		Cart userCart = new Cart();
+		String response = "";
+		
+		// Create Items() and add to Cart()
+		while (!isDonePurchasing) {
+			// create item being purchases: name, price, qty
+			System.out.println("Enter the name of the item: ");
+			String itemName = userInput.next();
+			
+			System.out.println("Enter the unit pric: ");
+			double itemUnitPrice = userInput.nextDouble();
+			
+			System.out.println("Enter quantity to purchase: ");
+			int itemQty = userInput.nextInt();
+			
+			// build and add item to cart
+			userCart.addItem(new Item(itemName, itemUnitPrice, itemQty));
+			userInput.nextLine(); // stop input until next input request.
+			
+			// display current cart contents
+			System.out.println("Current Cart:");
+			userCart.showOrder();
+			
+			// continue or end purchasing loop
+			System.out.println("\nContinue Shopping (y/n)?");
+			response = userInput.nextLine();
+			if (response.toLowerCase().equals("n") || response.toLowerCase().equals("no")) {
+				isDonePurchasing = true;
+			}
+			
+		} // end purchasing loop
+		
+		
+
+		
+		// TODO Option to remove items from cart
+		
+		
+
+	}
+
+}
